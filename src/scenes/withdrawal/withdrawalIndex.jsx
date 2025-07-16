7744f88622b36ece6d83b77c36df772b90a15d09
Joezy12 <108323742+Joezy12@users.noreply.github.com>
2025-07-16 15:35:17 +0100

    Update withdrawalIndex.jsx

--- src/scenes/withdrawal/withdrawalIndex.jsx
+++ src/scenes/withdrawal/withdrawalIndex.jsx
@@ -120,7 +120,7 @@ function Withdrawal() {
             <h1>withdraw</h1>
             <h2>${`${amount}`}</h2>
             <h1>withdrawal Fee</h1>
-            <h3>$25</h3>
+            <h3>${`${calFee}`}</h3>
 
             <div className="pay-btn">
                 <button className="debit" onClick={noCard} >Pay with Debit card/Credit card</button>
